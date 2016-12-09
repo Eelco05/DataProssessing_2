8cf4b6ab13f9db511e41ec9a6f288034b123f879
Eelco05 <eelco.alink@gmail.com>
2016-12-09 13:48:07 +0100

    Overzichtelijker
    Nog steeds moeilijk de data wisseling te implementeren

--- Homework_2016_2017/D3/d3lineExtended/d3lineExtended.js
+++ Homework_2016_2017/D3/d3lineExtended/d3lineExtended.js
@@ -1,38 +1,21 @@
+// setting chart dimentions
 var chart = d3.select("#linegraph"),
         WIDTH = 1000,
         HEIGHT = 500,
         MARGINS = { top: 10, right: 20, bottom: 10, left: 50 };
 
-d3.queue()
-    .defer(d3.json, 'd3lineExtendedLee.json')
-    .defer(d3.json, 'd3lineExtendedRot.json')
-    .defer(d3.json, 'd3lineExtendedBil.json')
-    .await(InitChart);
 
+// initialising chart
 function InitChart(error, data1, data2, data3) {
-
+    
     // scaling axis
     xScale = d3.scale.linear()
         .range([MARGINS.left, WIDTH - MARGINS.right])
-        .domain([
-            d3.min(data1, function(d) { return +d.dag; }), 
-            d3.max(data1, function(d) { return +d.dag; })]),
+        .domain(
+            d3.extent(data1, function(d) { return +d.dag; })
+        ),
         
     yScale = d3.scale.linear()
-        .range([HEIGHT - MARGINS.top, MARGINS.bottom])
-        .domain([
-            d3.min(data1, function(d) { return +d.dagMinTemp; }), 
-            d3.max(data1, function(d) { return +d.dagMaxTemp; })
-        ]),
-
-    yScaleData2 = d3.scale.linear()
-        .range([HEIGHT - MARGINS.top, MARGINS.bottom])
-        .domain([
-            d3.min(data2, function(d) { return +d.dagMinTemp; }), 
-            d3.max(data2, function(d) { return +d.dagMaxTemp; })
-        ]),
-
-    yScaleData3 = d3.scale.linear()
         .range([HEIGHT - MARGINS.top, MARGINS.bottom])
         .domain([
             d3.min(data3, function(d) { return +d.dagMinTemp; }), 
@@ -51,25 +34,14 @@ function InitChart(error, data1, data2, data3) {
         .outerTickSize(0)
         .tickPadding(10);       
 
-    // generating line
-    var line = d3.svg.line()
-      .x(function(d) { return xScale(+d.dag); })
-      .y(function(d) { return yScale(+d.dagGemTemp); })
-      .interpolate("linear");
-
-    // Define the div for the tooltip
-    var div = d3.select("body").append("div")   
-        .attr("class", "tooltip")               
-        .style("opacity", 0);
-
     // appending axis to chart
     chart.append("svg:g")
             .attr("class","axis")
             .attr("transform", "translate(0," + yScale(0) + ")")
             .call(xAxis.orient("bottom"))
         .append("text")
-            .attr("y", 120)
-            .attr("x", + MARGINS.left + 5)
+            .attr("y", -14)
+            .attr("x", + MARGINS.left + 3)
             .attr("dy", ".75em")
             .style("text-anchor", "start")
             .text("Day of the Month");
@@ -84,7 +56,7 @@ function InitChart(error, data1, data2, data3) {
             .attr("x", - MARGINS.top)
             .attr("dy", ".71em")
             .style("text-anchor", "end")
-            .text("Rainfall: Daily Average Temperature (0.1 C)");
+            .text("Temperature (0.1 Celcius)");
 
     // adding grid
     chart.append("svg:g")         
@@ -97,79 +69,100 @@ function InitChart(error, data1, data2, data3) {
             .attr("transform", "translate(" + (MARGINS.left) + ",0)")
             .call(yAxis.tickSize(-WIDTH + 60, 0, 0).tickFormat(""));
 
-    // line legend, doesn't work right and no longer nessisary with the tooltip
-    // chart.append("text")
-    //         .attr("x", xScale(31) + 5)
-    //         .attr("y", yScale(56) - 4)
-    //         .attr("dy", ".71em")
-    //         .style("text-anchor", "begin")
-    //         .style("font-size", "10px")
-    //         .style("fill", "green")
-    //         .text("Leeuwarden");
-
-    // chart.append("text")
-    //         .attr("x", xScale(31) + 5)
-    //         .attr("y", yScaleData2(76) - 3)
-    //         .attr("dy", ".71em")
-    //         .style("text-anchor", "begin")
-    //         .style("font-size", "10px")
-    //         .style("fill", "blue")
-    //         .text("Rotterdam");
-        
-    // chart.append("text")
-    //         .attr("x", xScale(31) + 5)
-    //         .attr("y", yScaleData3(68))
-    //         .attr("dy", ".71em")
-    //         .style("text-anchor", "begin")
-    //         .style("font-size", "10px")
-    //         .style("fill", "red")
-    //         .text("De Bilt");
-        
-
-    // appending data as lines to chart
-    function lines(data, color) {
-        chart.append('svg:path')
-        .attr('d', line(data))
-        .attr('stroke', color)
-        .attr('stroke-width', 2)
-        .attr('fill', 'none');
-    }
-
+    // adding data lines
     lines(data1, "green");
     lines(data2, "blue");
     lines(data3, "red");
 
+    // adding legend
+    chart.append("text")
+        .attr("x", xScale(29) + 2)
+        .attr("y", yScale(-42.5))
+        .attr("dy", ".71em")
+        .style("text-anchor", "begin")
+        .style("font-size", "12px")
+        .style("fill", "Black")
+        .text("Legend:");
+
+    legend(1, "green", "Leeuwarden")
+    legend(2, "blue", "Rotterdam")
+    legend(3, "red", "De Bilt")
+
 
     // adding dots and tooltip
-    function createDot (data, color) {
-
-        chart.selectAll("dot")   
-            .data(data)         
-        .enter().append("circle")                               
-            .attr("r", 2.2)       
-            .attr("cx", function(d) { return xScale(+d.dag); })       
-            .attr("cy", function(d) { return yScale(+d.dagGemTemp); })
-            .style("fill", color)
-            .on("mouseover", function(d) {      
-                div.transition()           
-                    .style("opacity", .9);      
-                div.html(
-                        "<strong>City: </strong>" + "<span>" + d.stad + "</span>" + "<br/>" +  
-                        // "<strong>Day of the Month: </strong>" + "<span>" + d.dag + "</span>" + "<br/>" +
-                        "<strong>Average Temperature: </strong>" + "<span>" + d.dagGemTemp + "</span>" + "<br/>" +
-                        "<strong>Minimum Temperature: </strong>" + "<span>" + d.dagMinTemp + "</span>" + "<br/>" +
-                        "<strong>Maximum Temperature: </strong>" + "<span>" + d.dagMaxTemp + "</span>")
-                    .style("left", (d3.event.pageX + 10) + "px")     
-                    .style("top", (d3.event.pageY + 10) + "px");    
-                })                  
-            .on("mouseout", function(d) {       
-                div.transition()        
-                    .duration(500)      
-                    .style("opacity", 0);   
-        });
-    }
-
     createDot(data1, "green");
     createDot(data2, "blue");
     createDot(data3, "red");
 }
+
+// importing json data
+d3.queue()
+    .defer(d3.json, 'd3lineExtendedLee.json')
+    .defer(d3.json, 'd3lineExtendedRot.json')
+    .defer(d3.json, 'd3lineExtendedBil.json')
+    .await(InitChart);
+
+
+// line plotter
+function lines(data, color) {
+    var line = d3.svg.line()
+        .x(function(d) { return xScale(+d.dag); })
+        .y(function(d) { return yScale(+d.dagGemTemp); })
+        .interpolate("linear");
+
+    chart.append('svg:path')
+        .attr('d', line(data))
+        .attr('stroke', color)
+        .attr('stroke-width', 2)
+        .attr('fill', 'none');
+}
+
+
+
+// Tooltip and linefots
+var div = d3.select("body").append("div")   
+    .attr("class", "tooltip")               
+    .style("opacity", 0);
+
+function createDot (data, color) {
+
+    chart.selectAll("dot")   
+        .data(data)         
+    .enter().append("circle")                               
+        .attr("r", 2.2)       
+        .attr("cx", function(d) { return xScale(+d.dag); })       
+        .attr("cy", function(d) { return yScale(+d.dagGemTemp); })
+        .style("fill", color)
+        .on("mouseover", function(d) {      
+            div.transition()           
+                .style("opacity", .9);      
+            div.html(
+                    "<strong>City: </strong>" + "<span>" + d.stad + "</span>" + "<br/>" + 
+                    "<strong>Average Temperature: </strong>" + "<span>" + d.dagGemTemp + "</span>" + "<br/>" +
+                    "<strong>Minimum Temperature: </strong>" + "<span>" + d.dagMinTemp + "</span>" + "<br/>" +
+                    "<strong>Maximum Temperature: </strong>" + "<span>" + d.dagMaxTemp + "</span>")
+                .style("left", (d3.event.pageX + 10) + "px")     
+                .style("top", (d3.event.pageY + 10) + "px");    
+            })                  
+        .on("mouseout", function(d) {       
+            div.transition()        
+                .duration(500)      
+                .style("opacity", 0);   
+    });
+}
+
+
+
+// legend 
+function legend(y, color, city) {
+    chart.append("text")
+        .attr("x", xScale(29) + 2)
+        .attr("y", yScale(-42.5 - ( y * 10)))
+        .attr("dy", ".71em")
+        .style("text-anchor", "begin")
+        .style("font-size", "12px")
+        .style("fill", color)
+        .text(city);
+}
+
+InitChart();
